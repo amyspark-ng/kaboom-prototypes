c649d9dd5a07d1641610e8d41f1a3fb430fbf7d1
amyspark-ng ✨ <amysparkng@gmail.com>
2024-05-14 17:48:55 -0500

    Squashed commit message (v0.1.2) just testing

--- newfiletest.js
+++ newfiletest.js
@@ -1,3 +1,5 @@
 // this is a new file test to work on the develop branch, this branch will not get updated with the main branch yet
 
 // i actually did another commmit!! this is git history test
+
+// have to do more commits!!!!
